576df9d0c21de946f1b68f6d968ccace4d4aad4a
yulim <yulim13sky@naver.com>
2021-11-23 22:31:30 +0900

    [FEAT] Lv3 3-1 DataBinding

--- app/src/main/java/kr/co/softcampus/sopt_assignment1/HomeActivity.kt
+++ app/src/main/java/kr/co/softcampus/sopt_assignment1/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import kr.co.softcampus.sopt_assignment1.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -14,10 +15,19 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        val intro = Introduce(
+            "최유림",
+            24,
+            "esfp",
+            "안녕하세요 SOPT 29기 \n안드로이드 YB입니다\n잘 부탁드립니다~♥\n\n안녕하세요 SOPT 29기 \n안드로이드 YB입니다\n잘 부탁드립니다~♥\n\n안녕하세요 SOPT 29기 \n안드로이드 YB입니다\n잘 부탁드립니다~♥\n\n안녕하세요 SOPT 29기 \n안드로이드 YB입니다\n잘 부탁드립니다~♥\n\n안녕하세요 SOPT 29기 \n안드로이드 YB입니다\n잘 부탁드립니다~♥"
+        )
+
+        binding.introduce = intro
+
         binding.git.setOnClickListener {
-            val url = "https://github.com/ChoiYuLim"
-            val webPage: Uri = Uri.parse(url)
+            val webPage: Uri = Uri.parse("https://github.com/ChoiYuLim")
             val intent = Intent(Intent.ACTION_VIEW, webPage)
 
             // 해당 intent를 성공적으로 수행할 수 있는지 체크
---
+++ app/src/main/java/kr/co/softcampus/sopt_assignment1/Introduce.kt
@@ -0,0 +1,8 @@
+package kr.co.softcampus.sopt_assignment1
+
+data class Introduce(
+    val name: String,
+    val age: Int,
+    val mbti: String,
+    val intro: String
+)
