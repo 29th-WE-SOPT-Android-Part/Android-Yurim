4b801862523019152da8da4552dd0fbeaa014e43
yulim <yulim13sky@naver.com>
2021-12-24 09:27:57 +0900

    [Feat]Lv1 Onboarding 화면 구현

---
+++ app/src/main/java/kr/co/softcampus/sopt_assignment1/Activity/OnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.softcampus.sopt_assignment1.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.softcampus.sopt_assignment1.R
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
---
+++ app/src/main/java/kr/co/softcampus/sopt_assignment1/Fragment/Onboarding1Fragment.kt
@@ -0,0 +1,32 @@
+package kr.co.softcampus.sopt_assignment1.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kr.co.softcampus.sopt_assignment1.R
+import kr.co.softcampus.sopt_assignment1.databinding.FragmentOnboarding1Binding
+
+class Onboarding1Fragment : Fragment() {
+    private var _binding: FragmentOnboarding1Binding? = null
+    private val binding get() = _binding ?: error("Binding 이 초기화 되지 않았습니다.")
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOnboarding1Binding.inflate(layoutInflater,container,false)
+        binding.btnNext.setOnClickListener{
+            findNavController().navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment)
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
